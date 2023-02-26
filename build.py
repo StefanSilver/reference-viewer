bc5fceb6ce1ac91356eb2504f61eb4c026068b8d
Adva Alkalay <adva.a@dataloop.ai>
2023-02-26 11:19:11 +0200

    test

--- build.py
+++ build.py
@@ -7,6 +7,12 @@ with open('./dataloop.json') as json_file:
     data = json.load(json_file)
     panel_name = data['components']['panels'][0]['name']
 
+print(f'Copying dist to panels/{panel_name}')
+src_dir = 'dist'
+dest_dir = f'panels/{panel_name}'
+files = os.listdir(src_dir)
+shutil.copytree(src_dir, dest_dir)
+
 index_path = f'./panels/{panel_name}/index.html'
 lines = []
 with open(index_path, 'r') as html_file:
--- build.sh
+++ build.sh
@@ -4,15 +4,9 @@ if test -d ./dist; then rm -r ./dist; fi
 if test -d ./panels/referenceViewer; then rm -r ./panels/referenceViewer; fi
 npm i
 npm run build
-cp -r ./dist ./panels/referenceViewer
-
-ls ./panels/
-ls ./panels/referenceViewer
-
+python3 ./build.py
 if test -d ./panels/referenceViewer; then echo '/panels/referenceViewer exists'; fi
-python ./build.py
 rm -r ./dist
-
 if test -d ./panels/referenceViewer/assets; then echo 'Copied dist succesfully'; fi
 echo $(ls)
 echo $(ls ./panels)
